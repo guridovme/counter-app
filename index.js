ed97571f047dbe8a08649afb5e731ed961d0f9c0
Nikolai Guridov <125973969+guridovme@users.noreply.github.com>
2023-05-13 19:25:21 +0200

    1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let button =document.querySelector('button');
+let counterText = document.querySelector('.counter');
 let counter = 0;
 
+
 button.addEventListener('click', function() {
-    counter=counter+1;
-    console.log(counter);
+    counter = counter+1;
+    counterText.innerText = counter;
 });
